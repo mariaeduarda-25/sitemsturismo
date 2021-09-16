0792194db072e91ecdf76b3e91f8f02da5f0b698
Maria Eduarda <sabinomaariaeduarda123@gmail.com>
2021-09-16 14:05:42 -0300

    site

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,4 +1,4 @@
-export {default as Home} from "./Home"
-export {default as Textos} from "./Textos"
-export {default as Indicacoes} from "./Indicações"
-export {default as Textos2} from "./Textos2"
+export {default as Home} from "./home"
+export {default as Textos} from "./textos"
+export {default as Indicacoes} from "./indicacoes"
+export {default as Textos2} from "./textos2"
